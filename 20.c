c3eb4c1b16d23002883fb38c67191880c40a2a8a
Sunil Kunwar <76618195+scnilkunwar@users.noreply.github.com>
2021-03-22 14:06:53 +0545

    Update 20.c

--- 20.c
+++ 20.c
@@ -19,3 +19,44 @@ int main()
 	}
 	return 0;
 }
+/* Multiplication Table of numbers in given range. */
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	int low_lim, up_lim;
+
+	
+
+	printf("enter lower limit: ");
+
+    scanf("%lu", &low_lim);
+
+    printf("enter upper limit: ");
+
+    scanf("%lu", &up_lim);
+
+   
+
+	for(int i=low_lim;i<=up_lim;i++)
+
+	{
+
+		printf("\nmultiplication table of %d\n",i);
+
+		for(int j=1;j<=10;j++)
+
+		{
+
+			printf("%d x %d = %d\n", j, i, j*i);
+
+		}
+
+	}
+
+	return 0;
+
+}
